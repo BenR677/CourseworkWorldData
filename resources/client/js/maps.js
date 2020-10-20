adddfaa49a332335c77886937f22a7edf931762a
ben <93576@farnborough.ac.uk>
2020-10-20 23:39:51 +0100

    Alot of stuff

--- resources/client/js/maps.js
+++ resources/client/js/maps.js
@@ -12,10 +12,10 @@ function prepareMap() {
 
     let cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>';
 
-    let positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+    let positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png', {
         attribution: cartodbAttribution
     }).addTo(map);
-
+//https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png
     let positronLabels = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
         attribution: cartodbAttribution,
         pane: 'labels'
@@ -26,6 +26,15 @@ function prepareMap() {
     geojson.eachLayer(function (layer) {
         layer.bindPopup(layer.feature.properties.ADMIN);
         /*alert('country = ' + bindPopup(layer.features.properties.ADMIN)); */
+
+        layer.on('click', function(e) {
+            console.log("cClick");
+            let countryName = layer.feature.properties.ADMIN;
+            //alert('Country clicked is ' + countryName);
+            runInfoCard(countryName);
+            //call function to retrieve data on this country
+        });
+
     });
 
     map.setView({lat: 47.040182144806664, lng: 9.667968750000002}, 4);
--- src/main/java/controllers/GetStats.java
+++ src/main/java/controllers/GetStats.java
@@ -14,21 +14,24 @@ import java.sql.ResultSet;
 @Produces(MediaType.APPLICATION_JSON)
 public class GetStats {
     @GET
-    @Path("getStats/{CountryID}")
-    public String countryStatsgetStats(@PathParam("CountryID") Integer CountryID) {
+    @Path("getStats/{NameEnglish}")
+    public String countryStatsgetStats(@PathParam("NameEnglish") String NameEnglish) {
         System.out.println("Invoked countryStats.getStats()");
         JSONArray response = new JSONArray();
         try {
-            PreparedStatement ps = Main.db.prepareStatement("SELECT CountryID, NameEnglish, NameNative, Languages, Population FROM CountryStats WHERE CountryID = ?");
-            ps.setInt(1,CountryID);
+            PreparedStatement ps = Main.db.prepareStatement("SELECT CountryID, NameEnglish, NameNative, Description, Languages, Population, LandArea, GrossDomProd FROM CountryStats WHERE NameEnglish = ?");
+            ps.setString(1,NameEnglish);
             ResultSet results = ps.executeQuery();
             while (results.next()==true) {
                 JSONObject row = new JSONObject();
                 row.put("CountryID", results.getInt(1));
                 row.put("NameEnglish", results.getString(2));
-                row.put("NameNative", results.getString(3));      //Blob?
-                row.put("Languages", results.getString(4));
-                row.put("Population", results.getInt(5));
+                row.put("NameNative", results.getString(3));
+                row.put("Description", results.getString(4));                                                          //Blob?
+                row.put("Languages", results.getString(5));
+                row.put("Population", results.getInt(6));
+                row.put("LandArea", results.getInt(7));
+                row.put("GrossDomProd", results.getInt(8));
                 response.add(row);
             }
             return response.toString();
